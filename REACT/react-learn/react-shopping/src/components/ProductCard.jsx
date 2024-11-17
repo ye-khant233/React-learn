34a619b6e6ed0ec2f44f0c5424fb388ef9ba21fa
YeKhant <yekhant791@gmail.com>
2024-11-17 16:30:15 +0530

    React Day 2 finish

--- REACT/react-learn/react-shopping/src/components/ProductCard.jsx
+++ REACT/react-learn/react-shopping/src/components/ProductCard.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
+import Rating from "./Rating";
 
-
-const ProductCard = () => {
+const ProductCard = ({
+  product: {
+    title,
+    price,
+    image,
+    rating: { rate },
+  },
+}) => {
   return (
-    <div>
-      ProductCard
-    </div>
-  )
-}
+    <div className="border border-black p-5 flex flex-col items-start gap-5">
+      <img src={image}  className="h-48" alt=""/>
+      <p className='font-bold line-clamp-2'>{title}</p>
+      <Rating rate={rate} />
+      <div className="flex justify-between items-center w-full mt-auto">
+      <p>{price}</p>
+      <button className=" text-sm border border-black px-3 py-1">Add cart</button>
 
-export default ProductCard
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
